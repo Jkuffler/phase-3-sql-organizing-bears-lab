66b5a5a22e20af53558e41b4fe4a389c5be567ef
Jason <jakuffler@gmail.com>
2021-09-23 19:04:11 -0400

    commit tree 39d9bde0f97dbde035fcf731cabe42b5363cd085

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 1, 'F', 'albino', 'aloof', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 8, 'M', 'brown', 'docile', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'black', 'egalitarian',0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 8, 'F', 'gray', 'militant', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 1, 'M', 'brown', 'queer', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 4, 'M', 'black', 'charitable', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 3, 'F', 'golden', 'darling', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 22, 'M', 'darker than midnight on a moonless night', 'mysterious', 0)
